3b5e097fdecafa8766c11631e3914c71584b5ae4
Dilandau <dilandau@mhonline.fr>
2012-11-23 19:11:42 +0100

    Init 2.14-MHO for 1.2.5 MCP

---
+++ src/main/java/com/frdfsnlght/inquisitor/handlers/PlayersHandler.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2012 frdfsnlght <frdfsnlght@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.frdfsnlght.inquisitor.handlers;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.frdfsnlght.inquisitor.DB;
+import com.frdfsnlght.inquisitor.PlayerStats;
+import com.frdfsnlght.inquisitor.Statistic;
+import com.frdfsnlght.inquisitor.TypeMap;
+import com.frdfsnlght.inquisitor.Utils;
+import com.frdfsnlght.inquisitor.WebRequest;
+import com.frdfsnlght.inquisitor.WebResponse;
+import com.frdfsnlght.inquisitor.WebServer;
+
+/**
+ * 
+ * @author frdfsnlght <frdfsnlght@gmail.com>
+ */
+
+public final class PlayersHandler extends TemplateHandler {
+
+	private static final String DEFAULT_COLUMNS = "level," + "deaths,"
+			+ "totalPlayersKilled," + "totalMobsKilled," + "totalBlocksBroken,"
+			+ "totalBlocksPlaced," + "totalItemsDropped,"
+			+ "totalItemsPickedUp," + "totalItemsCrafted,"
+			+ "totalDistanceTraveled," + "totalTime," + "online";
+
+	private static final String DEFAULT_SORT_COLUMN = "name";
+	private static final String DEFAULT_SORT_DIR = "ASC";
+
+	private static final String DEFAULT_RESTRICT_COLUMNS = "address,"
+			+ "coords," + "bedServer," + "bedWorld," + "bedCoords";
+
+	@Override
+	public void handleRequest(WebRequest req, WebResponse res)
+			throws IOException {
+		String defColumns = WebServer.getPlayersColumns();
+		if (defColumns == null)
+			defColumns = DEFAULT_COLUMNS;
+		String defSortColumn = WebServer.getPlayersSortColumn();
+		if (defSortColumn == null)
+			defSortColumn = DEFAULT_SORT_COLUMN;
+		String defSortDir = WebServer.getPlayersSortDir();
+		if (defSortDir == null)
+			defSortDir = DEFAULT_SORT_DIR;
+		String defRestrictColumns = WebServer.getPlayersRestrictColumns();
+		if (defRestrictColumns == null)
+			defRestrictColumns = DEFAULT_RESTRICT_COLUMNS;
+
+		String columnsStr = req.getParameter("columns", defColumns);
+		String sortColumn = req.getParameter("sortBy", defSortColumn);
+		String sortDir = req.getParameter("sortDir", defSortDir).toUpperCase();
+		int pageSize = req.getParameters().getInt("pageSize",
+				WebServer.getPlayersPageSize());
+		int pageNum = req.getParameters().getInt("page", 1);
+		String playerName = req.getParameter("playerName", null);
+
+		Set<String> restrictColumns = new HashSet<String>(
+				Arrays.asList(defRestrictColumns.split("\\s*,\\s*")));
+
+		Set<String> allColumns = new HashSet<String>();
+		for (String statName : PlayerStats.group.getStatisticsNames()) {
+			Statistic stat = PlayerStats.group.getStatistic(statName);
+			if (stat.isMapped())
+				continue;
+			switch (stat.getType()) {
+			case STRING:
+			case BOOLEAN:
+			case TIMESTAMP:
+			case ELAPSED_TIME:
+			case DISTANCE:
+			case INTEGER:
+			case LONG:
+			case FLOAT:
+			case DOUBLE:
+				if (!restrictColumns.contains(statName))
+					allColumns.add(statName);
+				break;
+			}
+		}
+
+		List<String> columns = new ArrayList<String>();
+		for (String column : columnsStr.split("\\s*,\\s*"))
+			if (allColumns.contains(column))
+				columns.add(column);
+		if (!columns.contains(sortColumn))
+			sortColumn = DEFAULT_SORT_COLUMN;
+		if ((!sortDir.equals("ASC")) && (!sortDir.equals("DESC")))
+			sortDir = DEFAULT_SORT_DIR;
+		if (pageSize < 1)
+			pageSize = 1;
+		if (pageNum < 1)
+			pageNum = 1;
+		if (playerName != null) {
+			playerName = playerName.trim();
+			if (playerName.isEmpty())
+				playerName = null;
+		}
+
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			StringBuilder sql = new StringBuilder();
+			sql.append("SELECT COUNT(1) FROM ").append(
+					DB.tableName(PlayerStats.group.getName()));
+			if (playerName != null)
+				sql.append(" WHERE `name` LIKE ?");
+			stmt = DB.prepare(sql.toString());
+			if (playerName != null)
+				stmt.setString(1, "%" + playerName + "%");
+			rs = stmt.executeQuery();
+			rs.next();
+			int totalPlayers = rs.getInt(1);
+			rs.close();
+			stmt.close();
+
+			int totalPages = (int) Math.ceil((double) totalPlayers
+					/ (double) pageSize);
+			if (totalPages < 1)
+				totalPages = 1;
+			if (pageNum > totalPages)
+				pageNum = totalPages;
+			int limitOffset = (pageNum - 1) * pageSize;
+
+			sql = new StringBuilder();
+			sql.append("SELECT `name`");
+			for (String column : columns)
+				sql.append(',').append(column);
+			sql.append(" FROM ").append(
+					DB.tableName(PlayerStats.group.getName()));
+			if (playerName != null)
+				sql.append(" WHERE `name` LIKE ?");
+			sql.append(" ORDER BY `").append(sortColumn).append("` ")
+					.append(sortDir);
+			if (!"name".equals(sortColumn))
+				sql.append(", `name` ASC");
+			sql.append(" LIMIT ").append(limitOffset).append(",")
+					.append(pageSize);
+			stmt = DB.prepare(sql.toString());
+			if (playerName != null)
+				stmt.setString(1, "%" + playerName + "%");
+			rs = stmt.executeQuery();
+
+			List<TypeMap> players = new ArrayList<TypeMap>(pageSize);
+			while (rs.next()) {
+				TypeMap player = PlayerStats.group.loadStatistics(rs, columns);
+				player.set("name", rs.getString("name"));
+				players.add(player);
+			}
+
+			TypeMap data = new TypeMap();
+			data.put("totalPlayers", totalPlayers);
+			data.put("totalPages", totalPages);
+			data.put("page", pageNum);
+			data.put("pageSize", pageSize);
+			data.put("showColumns", columns);
+			data.put("sortBy", sortColumn);
+			data.put("sortDir", sortDir);
+			data.put("players", players);
+			data.put("firstPlayerOffset", limitOffset);
+			data.put("playerName", playerName);
+
+			List<String> hideColumns = new ArrayList<String>();
+			for (String col : allColumns)
+				if (!columns.contains(col))
+					hideColumns.add(col);
+			data.put("hideColumns", hideColumns);
+
+			renderTemplate(req, res, "resources/players.ftl", data);
+
+		} catch (SQLException se) {
+			res.setStatus(500, "Internal Server Error");
+			Utils.severe("SQLException while selecting players: %s",
+					se.getMessage());
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se) {
+			}
+		}
+	}
+
+}
